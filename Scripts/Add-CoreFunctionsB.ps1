e3c12e9db1ed7a41fb14cdff3b5372e7189570d3
Bryan Klumpp <klumpp6@gmail.com>
2022-05-23 06:35:18 -0500

    backup and web search enhancements

--- Scripts/Add-CoreFunctionsB.ps1
+++ Scripts/Add-CoreFunctionsB.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Web
+Add-Type -AssemblyName System.Windows.Forms
 
 
 $myPath = Get-Item $MyInvocation.MyCommand.Path
@@ -6,7 +7,7 @@ $myDir = $myPath.Directory
 
 #--------------- Set Color Scheme - begin ------------------
 $ANSIColorSequenceBW="$([char]0x1b)[38;2;0;0;0;48;2;255;255;255;m"
-Function colorMaster() {
+Function bw() {
     $host.ui.rawui.ForegroundColor = 'Black'
     $host.ui.rawui.BackgroundColor = 'White'
     Set-PSReadLineOption -Colors @{
@@ -40,7 +41,6 @@ Function colorMaster() {
  
     #Note that the overriden Prompt method fixes the remaining color piece
 }
-colorMaster   #doitnow
 
 function Prompt   #Override default prompt to set color scheme
 {
@@ -107,11 +107,22 @@ function whatisitO() {
     }
     return $s
 }
-
-function w() {
-    se "https://www.google.com/search?q=_searchFor_" $args
+#/table/incident?sysparm_query=sys_id=bbc2ddcedba51300b2bd711ebf961944^category=enquiry&sysparm_exclude_reference_link=true&sysparm_view=false&sysparm_display_value=false&sysparm_suppress_pagination=false&sysparm_limit=500  https://community.servicenow.com/community?id=community_question&sys_id=a3a50c1edbd7d3001cd8a345ca961942  IP1
+function inc() {
+    searchByURL 'https://<instance name>.service-now.com/incident_list.do?HTML&sysparm_query=priority=1&sysparm_orderby=assigned_to'
 }
-function se() { 
+function task() {
+    searchByURL 'https://<instance name>.service-now.com/task_list.do?HTML&sysparm_query=priority=1&sysparm_orderby=assigned_to'
+}
+function w() {
+    searchByURL "https://www.google.com/search?q=_searchFor_" $args
+}
+New-Alias dellSupport -Name d
+function  dellSupport() {
+    AltTab; CtrlC; Start-Process "https://www.dell.com/support/home/en-us"
+}
+New-Alias -Name se searchByURL
+function searchByURL() { 
     #Add-Type -AssemblyName System.Web  #included at beginning of file?
     $base      = $args[0]
     $searchStrings = @($args[1])
@@ -135,10 +146,11 @@ Function AdminShell() {
 }
 
 Function robobak() {
-    $bakDir='c:\bak'
+    $bakDir='c:\backup'
     $userProfile=$env:USERPROFILE
     md $bakDir  #make the filename shorter than c:\users to attempt to avoid total path length issues, although still possible
     # not using /Z, possible better performance, TODO measure it; also re-evaluate /B, may need admin elevation
+    # remove /SEC if wanting to execute with non-admin user
     $cmdString = "`@echo off`r`nrobocopy $userProfile $bakDir /E /SEC /SJ /SL /DCOPY:DAT /XD `"$userProfile\AppData`" /XJ /R:0 /W:0 /V /FP /LOG:$bakDir\robocopy_log.txt /TEE`necho Please screenshot this text and send to IT if there are any questions about whether it was totally successful, especially if more than 0 files show up as FAILED`r`npause`r`npause`r`npause"
     $bat = ($bakDir + "\_go.bat")
     Write-Output $cmdString | Out-File -Encoding utf8 $bat
@@ -149,14 +161,25 @@ Function robobak() {
 function AltTab() {
     [System.Windows.Forms.SendKeys]::SendWait("%({TAB})")
 }
-
-function AltTabPasteText() {
+function AltTabTab() {
+    [System.Windows.Forms.SendKeys]::SendWait("%({TAB}{TAB})")
+}
+function CtrlC() {
+    [System.Windows.Forms.SendKeys]::SendWait("^c")
+}
+function CtrlV() {
+    [System.Windows.Forms.SendKeys]::SendWait("^v")
+}
+function AltTabPasteArg0() {
     $text = $args[0]
     set-clipboard $text
     AltTab
-    [System.Windows.Forms.SendKeys]::SendWait("^v")
+    CtrlV
 }
 
 function nitt() {
-    AltTabPasteText "Now is the time`nfor all good men to come to the aid of their country."
+    AltTabPasteArg0 "Now is the time`nfor all good men to come to the aid of their country."
 }
+
+#startup
+bw
---
+++ Scripts/backup_non_admin_default.bat
@@ -0,0 +1,6 @@
+﻿mkdir c:\backup
+robocopy %USERPROFILE% c:\backup /E /SJ /SL /DCOPY:DAT /XD "C:\Users\b\AppData" /XJ /R:0 /W:0 /V /FP /LOG:c:\backup\robocopy_log.txt /TEE
+echo Please screenshot this text and send to IT if there are any questions about whether it was totally successful, especially if more than 0 files show up as FAILED
+pause
+pause
+pause
