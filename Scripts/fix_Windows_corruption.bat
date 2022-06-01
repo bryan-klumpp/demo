bc302bd129984782fa7853c62abd4ce9e972ac84
Bryan Klumpp <klumpp6@gmail.com>
2022-06-01 06:36:09 -0500

    more polished bat

--- Scripts/fix_Windows_corruption.bat
+++ Scripts/fix_Windows_corruption.bat
@@ -1,23 +1,23 @@
-mkdir %USERPROFILE%\it
-set logfile=%USERPROFILE%\it\fix_Windows_corruption_bat.log
+::see https://support.microsoft.com/en-us/topic/use-the-system-file-checker-tool-to-repair-missing-or-corrupted-system-files-79aa86cb-ca52-166a-92a3-966e85d4094e for why sfc is run after dism
+mkdir %USERPROFILE%\IT
+set logfile=%USERPROFILE%\IT\fix_Windows_corruption_bat_%RANDOM%.log
 @echo off
-echo
-echo
-echo This batch job runs chkdsk c:, and if it succeeds it then runs a sequence of sfc, dism, and sfc again for good measure.  The window can run in the background while the user works.  It will not show the key output to the console window, but instead log it to the file %logfile%  This window will close itself when finished; please do not restart, sleep, or shutdown, if possible, until this window disappears.
+echo " "
+echo "------------------------------------------------------"
+echo This batch job runs chkdsk c:, and if it succeeds it then runs a sequence of sfc, dism, and sfc again.  The window can run in the background while the user works.  It will not show the detailed output to this window, but instead log it to the file %logfile%  If possible, please do not restart, sleep, or shutdown until this window either says it is finished or disappears, and notify analyst when the window either says it is finished or disappears.
+echo "------------------------------------------------------"
 @echo on
 chkdsk c: >> %logfile% 2>&1
 if %ERRORLEVEL% neq 0 goto LabelChkdskFail
 sfc /scannow >> %logfile% 2>&1
 dism /online /cleanup-image /restorehealth >> %logfile% 2>&1
 sfc /scannow >> %logfile% 2>&1
-
 @echo off
-:: This section is commented out for now, might be useful if wanting to see the output in the console window for whatever reason
-:: echo If possible, do not press any key (despite what is printed below), ideally contact analyst to review this, minimize the window for now.  If you need to restart or shut down, if possible, drag your cursor from the lower-right corner to the very top-left corner of the screen to highlight all text, press Ctrl + C, and paste into email to analyst.  
-:: pause
-:: pause
-:: pause
+echo "------------------------------------------------------"
+echo Jobs finished running, please notify analyst
+echo "------------------------------------------------------"
 @echo on
+pause
 
 goto End
 
