9f0f71918c0ffa32e3549145848dcb40e1abe65f
Bryan Klumpp <klumpp6@gmail.com>
2022-06-23 07:12:44 -0500

    x

--- Scripts/nestnesting/runas_main.ps1
+++ Scripts/nestnesting/runas_main.ps1
@@ -1,1 +1,3 @@
 start-process -FilePath powershell -ArgumentList "-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process; . C:\Users\b\Documents\WindowsPowerShell\nestnesting\runas_sub.ps1; pause"
+#from sub just FYI: start-process -FilePath cmd -Verb runas -WindowStyle maximized
+#from sub just FYI: start-process -FilePath cmd -Verb runas -WindowStyle max
