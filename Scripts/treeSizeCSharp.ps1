76277d562e658b7d00016c4e49376643bebdb5d8
Bryan Klumpp <klumpp6@gmail.com>
2022-11-29 22:59:56 -0600

    Update treeSizeCSharp.ps1

--- Scripts/treeSizeCSharp.ps1
+++ Scripts/treeSizeCSharp.ps1
@@ -1,4 +1,13 @@
-﻿
+﻿$localCSfile=($env:USERPROFILE + "\TreeSize.cs")  #'c:\users\b\source\repos\ConsoleApp1\ConsoleApp1\Program.cs'
+
+if(false) {
+    $githuburl = 'https://raw.githubusercontent.com/bryan-klumpp/demo/main/Scripts/TreeSize.cs'
+    ((Invoke-WebRequest $githuburl).Content) > $localCSfile
+    #PLEASE review the downloaded code
+    Get-Content $localCSfile
+}
+
+#Add-Type -TypeDefinition (get-content 'c:\users\b\source\repos\ConsoleApp1\ConsoleApp1\Program.cs' | out-string)
 Add-Type -TypeDefinition (get-content 'c:\users\b\source\repos\ConsoleApp1\ConsoleApp1\Program.cs' | out-string)
 
 [FileUtils.FileTreeSize]::treeSize('c:\')
