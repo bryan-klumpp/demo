7510c9d81b34dbced419429a9023b19bd07b1163
bryan-klumpp <klumpp6@gmail.com>
2023-02-27 15:43:45 -0600

    updating shell scripts for automatic Ubuntu setup

--- Scripts/sh/setupUbuntuDevEnv.sh
+++ Scripts/sh/setupUbuntuDevEnv.sh
@@ -3,6 +3,7 @@ mygeneratedlnkdir=/l
 mygitusername=bryan-klumpp
 mygitemail=klumpp6@gmail.com
 mylanid=klumpb
+myDefaultArchiveDir=/b/b22_archive
 
 wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O ~/Downloads/vscode.deb
 sudo apt install ~/Downloads/vscode.deb
@@ -22,6 +23,9 @@ shdir="${myworkspace}/bbase/Scripts/sh"
      chmod -R 750       $shdir
 sudo chown -R root:${mylanid} $shdir
 sudo ln -s $shdir $shlnk
+test -e ~/.bash_aliases || { echo '. /sh/b13_.bash_aliases.sh' > ~/.bash_aliases; }
+test -e /b || { sudo mkdir /b; sudo chown ${mylanid}:${mylanid} /b; }
+test -e ${myDefaultArchiveDir} || { sudo mkdir ${myDefaultArchiveDir}; sudo chown ${mylanid}:${mylanid} ${myDefaultArchiveDir}; }
 
 #Sublime installation as per https://www.sublimetext.com/docs/linux_repositories.html
 #note, this step may prompt you for 
@@ -29,3 +33,7 @@ curl -s https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sud
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+test -e /mnt || { sudo mkdir /mnt; }
+test -e /mnt/hgfs || { sudo mkdir /mnt/hgfs; }
+
