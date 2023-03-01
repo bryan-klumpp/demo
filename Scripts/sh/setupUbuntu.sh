114f8f3152ea16af5d74e0f4fb5c351fd1e14c79
bryan-klumpp <klumpp6@gmail.com>
2023-03-01 13:31:25 -0600

    just a comment

--- Scripts/sh/setupUbuntu.sh
+++ Scripts/sh/setupUbuntu.sh
@@ -27,6 +27,7 @@ test -e ~/.bash_aliases || { echo ". ${shlnk}/b13_.bash_aliases.sh" > ~/.bash_al
 test -e ${myDefaultArchiveDir} || { sudo mkdir ${myDefaultArchiveDir}; sudo chown ${myusername}:${myusername} ${myDefaultArchiveDir}; } &&
 
 #Sublime installation as per https://www.sublimetext.com/docs/linux_repositories.html
+#note, this part of the script may prompt for gpg password(s)
 sudo apt-get install sublime-text || {
      test -e /etc/apt/trusted.gpg.d/sublimehq-archive.gpg || { curl -s https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null; } &&
      echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
