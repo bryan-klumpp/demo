ba9df4e2c0d5348c69a4b3122755be125226a588
bryan-klumpp <klumpp6@gmail.com>
2023-03-06 02:01:42 -0600

    rename dn to don, I want "dn" alias for dotnet stuff and made it remove syntax coloring because it was messing with my black-on-yellow terminal

--- Scripts/sh/dn.sh
+++ Scripts/sh/dn.sh
@@ -1,5 +1,1 @@
-for i; do {
-  markeddone=$(sql1 "select desc from task where i = $i") || return 33
-  isval $markeddone && sql "update task set remain = 0 where i = "$i &&
-     echo '\nmarked done: '$(highlight "$markeddone") || return 83
-} done
+dotnet "$@" 2>&1 | nocolor
