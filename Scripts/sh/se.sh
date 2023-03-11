116668bc20170612bbbe244ff5255a46f6ace766
bryan-klumpp <klumpp6@gmail.com>
2023-03-11 12:09:45 -0600

    go

--- Scripts/sh/se.sh
+++ Scripts/sh/se.sh
@@ -5,8 +5,8 @@ browser=firefox
 isequal "$1" f && { browser=firefox; shift 1; }
 jnl "searched for $*"
 searchstr="$(j urlencode "$*")"
-#url='https://www.google.com/search?q='"$searchstr"
-url='https://duckduckgo.com/html/?q='"$searchstr"
+url='https://www.google.com/search?q='"$searchstr"
+#url='https://duckduckgo.com/html/?q='"$searchstr"
 #wd "$url" ; return
 a $browser "$url"; 
 return
