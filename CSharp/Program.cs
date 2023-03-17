d02973a266ab4e588413430beb7861deb63152c5
bryan-klumpp <klumpp6@gmail.com>
2023-03-16 22:23:31 -0500

    infer XML schema playground

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+using System.Xml.Schema;
+
 Console.WriteLine("Hello, World!");
+System.Xml.XmlReader reader = XmlReader.Create(@"/home/klumpb/note.xml");
+XmlSchemaSet schemaSet = new XmlSchemaSet();
+XmlSchemaInference schema = new XmlSchemaInference();
+// make 
+schema.Occurrence = XmlSchemaInference.InferenceOption.Restricted;
+schema.TypeInference = XmlSchemaInference.InferenceOption.Restricted;
+schemaSet = schema.InferSchema(reader);
+
+foreach (XmlSchema s in schemaSet.Schemas())
+{
+    using (var stringWriter = new StringWriter())
+    {
+        using (var writer = XmlWriter.Create(stringWriter))
+        {
+            s.Write(writer);
+        }
+
+        Console.WriteLine( stringWriter.ToString() );
+    }
+}
