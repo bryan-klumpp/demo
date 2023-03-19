3cc2021be6635f39cdbdbaf766813994f0d22a48
bryan-klumpp <klumpp6@gmail.com>
2023-03-19 18:00:57 -0500

    XML2 shows no schema errors but will not validate foreign keys

--- Scripts/sh/te.sh
+++ Scripts/sh/te.sh
@@ -1,6 +1,6 @@
 #which leafpad || { echo 'installing leafpad...'; inst leafpad || return 35; }
 file="$*"
-testf "$file" || file=$(echon "$file"|tr ' ' '_').txt
+testf "$file" || file=$(echon "$file"|tr ' ' '_')
 #testf "$file" || file=$(paste)
 #testf "$file" || { file="$(bdate)_$file"; }
 #nano  --syntax=none  "$file"  #--mouse
