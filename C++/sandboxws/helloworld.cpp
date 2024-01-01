eedd827433e3d7105699d39f381182c87b15cd15
bryan-klumpp <klumpp6@gmail.com>
2024-01-01 02:30:50 -0600

    before making things const

--- C++/sandboxws/helloworld.cpp
+++ C++/sandboxws/helloworld.cpp
@@ -20,42 +20,53 @@ template <typename T> T bitwiseNOT(T i) {
     static_assert(std::is_unsigned<T>::value && std::is_integral<T>::value, "T must be unsigned integral type (integer, long, etc.)");
     return ~i;
 }
+void playWithFunctionByConstPointer(const string* s) {
+    cout << "stringSandbox: " << s << endl;
+}
+void playWithFunctionByConstReference(const string& s) {
+    cout << "stringSandbox: " << s << endl;
+}
+
 void ampersandAsteriskFun() { //https://stackoverflow.com/questions/6877052/use-of-the-operator-in-c-function-signatures
-        string s = "Hello, wordl";
-        string ss = "Hello, wordl";
+        string asdf = "asdf";
+        //playWithFunction(*asdf);
+        //playWithFunction(&asdf);
+        string newasdf = "asdf";
         string tt = "Helloooooooo, wordl";
         string autos = "Hello, wordl";
-        string* p = &s; // Here you get an address of s
-        string* pp = &ss; // Here you get an address of s
+        string* p = &asdf; // Here you get an address of asdf
+        playWithFunctionByConstPointer(p);
+        //playWithFunctionByConstReference(asdf);
+        string* pp = &newasdf; // Here you get an address of asdf
         string* pnullptr = nullptr;
         cout << "eg1" << (p == nullptr) << endl;
         //cout << "eg1.5" << (*p == *pnullptr) << endl; //segfault
-        cout << "eg2" << (s == ss) << endl;
+        cout << "eg2" << (asdf == newasdf) << endl;
         cout << "eg3" << (p == pp) << endl;
         cout << "eg3.5" << (*p == *pp) << endl;
-        cout << "eg4" << (ss == tt) << endl;
-        string& r = s; // Here, r is a reference to s  
-        auto saddress = (string*)&s;
+        cout << "eg4" << (newasdf == tt) << endl;
+        string& r = asdf; // Here, r is a reference to asdf  
+        auto saddress = (string*)&asdf;
         string* rereferenced = saddress;
-        auto divergentcopy = s;  
-        auto autop = &s;
+        auto divergentcopy = asdf;  
+        auto autop = &asdf;
         string& makereferencefrompointer = *&*&*&*&*&*&*&*&*&*&*&*&*p; // compiler abuse: this is to make a copy using a pointer
         string& makereferencefromautop = *&*&*&*&*&*&*&*&*&*&*&*&*autop; // compiler abuse: this is to make a copy using a pointer
         string makecopyfrompointer = *p; // this is to make a copy using a pointer
 
-        s = "Hello, world"; // corrected
-        assert( s == *p ); // this should be familiar to you, dereferencing a pointer
-        assert( s == r ); // this will always be true, they are twins, or the same thing rather
+        asdf = "Hello, world"; // corrected
+        assert( asdf == *p ); // this should be familiar to you, dereferencing a pointer
+        assert( asdf == r ); // this will always be true, they are twins, or the same thing rather
 
         string copy1 = *p; // this is to make a copy using a pointer
         string copy = r; // this is what you saw, hope now you understand it better.
 
-        cout << "s: " << s << endl;
-        debugline(s)
+        cout << "asdf: " << asdf << endl;
+        debugline(asdf)
         debugline(autos)
-        cout << "&s: " << &s << endl;
-        //debugline(#&s)
-        cout << "*&s: " << *&s << endl;
+        cout << "&asdf: " << &asdf << endl;
+        //debugline(#&asdf)
+        cout << "*&asdf: " << *&asdf << endl;
         cout << "p: " << p << endl;
         debugline(p)
         debugline(autop)
