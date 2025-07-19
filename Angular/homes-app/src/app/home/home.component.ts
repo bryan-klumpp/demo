b44ac9f363967d5aca4fd97965c3c0173ec73620
Bryan Klumpp <klumpp6@gmail.com>
2025-07-19 16:29:04 -0500

    progress I think

--- Angular/homes-app/src/app/home/home.component.ts
+++ Angular/homes-app/src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  //imports: []
+  imports: [CommonModule, HousingLocationComponent],
   template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styleUrls: ['./home.component.css']
+    <section><form><input type="text" placeholder="Filter by city"><button class="primary">Search</button></form></section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+   `,
+  styleUrls: ['./home.component.css'],
+
 })
 export class HomeComponent {
 
---
+++ Angular/homes-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Photo of {{ housingLocation.location }}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }},{{ housingLocation.state }}</p>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+
+}
