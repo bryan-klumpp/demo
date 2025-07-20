c4c53d6afc0a6ae4a138145fd5b3fdd1f54d79bd
Bryan Klumpp <klumpp6@gmail.com>
2025-07-19 20:36:02 -0500

    making progress quicker

--- Angular/first-app/src/app/home/home.ts
+++ Angular/first-app/src/app/home/home.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import { HousinglocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   template: `
-    <p>
-      home works!
-    </p>
+    <section><form><input type="text" placeholder="Filter by city" /><button class="primary" type="button">Search</button></form></section>
+    <section class="results">
+      <app-housing-location></app-housing-location></section>
   `,
-  styles: ``
+  styleUrls: ['./home.css'],
 })
 export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+housingLocation: HousinglocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
--- Angular/homes-app/src/app/housing-location/housing-location.component.ts
+++ Angular/homes-app/src/app/housing-location/housing-location.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Photo of {{ housingLocation.location }}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city }},{{ housingLocation.state }}</p>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
