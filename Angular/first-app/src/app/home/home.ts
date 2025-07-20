3f3259ed2a2a3bee40c5b3a8892a45d9f6b0a01f
Bryan Klumpp <klumpp6@gmail.com>
2025-07-19 20:50:46 -0500

    progress

--- Angular/first-app/src/app/home/home.ts
+++ Angular/first-app/src/app/home/home.ts
@@ -8,7 +8,7 @@ import { HousinglocationInfo } from '../housinglocation';
   template: `
     <section><form><input type="text" placeholder="Filter by city" /><button class="primary" type="button">Search</button></form></section>
     <section class="results">
-      <app-housing-location></app-housing-location></section>
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location></section>
   `,
   styleUrls: ['./home.css'],
 })
