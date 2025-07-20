827faf7c0f26e01bc5c033ddd519367756afc029
Bryan Klumpp <klumpp6@gmail.com>
2025-07-19 22:22:32 -0500

    x

--- Angular/first-app/src/app/housing-location/housing-location.ts
+++ Angular/first-app/src/app/housing-location/housing-location.ts
@@ -1,15 +1,18 @@
 import { Component,input } from '@angular/core';
 import { HousingLocationInfoInterface } from '../housinglocation';
+import { Router, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink],
   template: `
     <section class="listing"><img class="listing-photo" 
     [src]="housingLocation().photo" alt="Photo of {{housingLocation().name}}"
     crossorigin />
     <h2 class="listing-heading">{{housingLocation().name}}</h2>
     <p class="listing-location">{{housingLocation().city}}, {{housingLocation().state}}</p>
+    <a [routerLink]="['/details', housingLocation().id]">Learn more</a>
     </section>
   `,
   styleUrls: ['./housing-location.css'],
