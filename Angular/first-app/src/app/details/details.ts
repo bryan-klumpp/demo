d3b2ce21b6cc6b8d20d64a52aaaf6fe757e734a9
Bryan Klumpp <klumpp6@gmail.com>
2025-07-20 00:44:13 -0500

    x

--- Angular/first-app/src/app/details/details.ts
+++ Angular/first-app/src/app/details/details.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing';
+import { HousingLocationInfoInterface } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   imports: [],
   template: `
     <p>
-      details works!
+      details works!  {{housingLocationId}}
     </p>
   `,
   styles: ``
 })
 export class Details {
+route:ActivatedRoute = inject(ActivatedRoute);
+housingLocationId = -1;
+constructor(){
+this.housingLocationId = Number(this.route.snapshot.params['id']);
 
+}
 }
