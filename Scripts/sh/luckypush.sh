ca8e1087aaccca95151e3a61f17cf7dcd278d3c2
Bryan Klumpp <klumpp6@gmail.com>
2025-07-21 22:58:18 -0500

    I will not dignify this code with a comment

--- Scripts/sh/luckypush.sh
+++ Scripts/sh/luckypush.sh
@@ -1,7 +1,11 @@
   git status
+  echo
   echo "Press Ctrl+C within 5 seconds to cancel push..."
-  sleep 5 && {
+  { sleep 5 &&
   git add * &&
-  git commit -m "Sorry, I'm being lazy" &&
+  git commit -m "I will not dignify this code with a comment" &&
   git push
+} || {
+  echo "Push cancelled or failed." #not sure why this won't print on cancel?
 }
+
