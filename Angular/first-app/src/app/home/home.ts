286a1d6126530255b8d8f319c42fc05590e14a05
Bryan Klumpp <klumpp6@gmail.com>
2025-07-23 00:38:17 -0500

    hijacking home page for debounce test

--- Angular/first-app/src/app/home/home.ts
+++ Angular/first-app/src/app/home/home.ts
@@ -10,14 +10,20 @@ import { debounceTime, switchMap, takeUntil, tap, delay } from 'rxjs/operators';
   imports: [HousingLocation],
   template: `
     <section><form>
-      <input type="text" [value]="changeCounter" placeholder="Filter by city" #changeCounterInput readonly/>
       <!-- https://www.google.com/search?q=button+debounce+using+angular+pipe&oq=button+debounce+using+angular+pipe&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDg0MjJqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8 -->
-      <!--                                                               (I used [disabled] but could use the [busy] property which we have in our app?) -->
-      <button class="primary" type="button" (click)="buttonTapped(changeCounterInput.value)">Debounce Test</button></form></section>
-    <section class="results">
-      @for(nextHousingLocation of filteredLocationList; track $index) {
-        <app-housing-location [housingLocation]="nextHousingLocation"></app-housing-location>
-      }
+      <input type="text" [value]="changeCounter" placeholder="Filter by city" #changeCounterInput readonly/>
+
+    <!-- This button will be debounced, meaning it will block for one second after every click.
+         There are, unfortunately, two different means of the word "debounce" on the Internet,
+         the difference being whether or not the intended action is executed immediately if there 
+         were no recent clicks.  In this case, yes, we want it to take action immediately if there 
+         were no recent clicks.  The button simply increments the value in the input box.  -->
+    <button class="primary" type="button" (click)="buttonTapped(changeCounterInput.value)">Debounce Test</button></form></section>
+
+    <section class="results"> 
+        @for(nextHousingLocation of filteredLocationList; track $index) {
+          <app-housing-location [housingLocation]="nextHousingLocation"></app-housing-location>
+        }
     </section>
   `,
   styleUrls: ['./home.css'],
@@ -29,21 +35,20 @@ export class Home implements OnInit, OnDestroy {
     isButtonDebounced = false;
 
     buttonTapped(textValue: string) {
-       console.log('Button tapped');
-       this.buttonClicks.next();
+       this.buttonClicks.next();                   // Kick off the pipe chain
     }
     ngOnInit(): void {
       this.buttonClicks.pipe(
         takeUntil(this.destroy$),                  // Unsubscribe when the component is destroyed
-        tap(() => this.doRightAway()),             // This function will check the debounce status and actually do something useful if not debounced
+        tap(() => this.doRightAway()),             // This function will check the debounce status and actually do something useful ONLY if not debounced
         debounceTime(1000),                        // Wait for 1 second of inactivity before resetting the debounce state
         tap(() => this.isButtonDebounced = false), // Reset the debounce state
       ).subscribe();
     }
     doRightAway() {
         if( ! this.isButtonDebounced ) {
-          this.changeCounter = (parseInt(this.changeCounter, 10) + 1).toString(); // This is the actual stuff we want the button to do, in this case just incrementing a value displayed on screen
-          this.isButtonDebounced = true;           // Prevent button taps from doing anything for another second after a tap
+          this.changeCounter = (Number(this.changeCounter) + 1).toString(); // This is the actual stuff we want the button to do, for example incrementing a value displayed on screen
+          this.isButtonDebounced = true;            // Prevent button taps from doing anything for another second after a tap
         }
     }
     ngOnDestroy(): void {
