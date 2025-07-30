9d91cc98a548031d2314726e5a5acb05de3141b7
Bryan Klumpp <klumpp6@gmail.com>
2025-07-29 23:49:29 -0500

    I will not dignify this code with a comment

--- Scripts/sh/luckypush.sh
+++ Scripts/sh/luckypush.sh
@@ -2,6 +2,7 @@
   echo
   echo "Press Ctrl+C within 5 seconds to cancel push..."
   { sleep 5 &&
+   cd /c/b/code/demo &&
   (git add *) &&
   git commit -m "I will not dignify this code with a comment" &&
   git push && echo 'pushed!'
