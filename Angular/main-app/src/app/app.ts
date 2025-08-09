aa9c49ec687a23feae8314196083c7fac2bfb593
Bryan Klumpp <klumpp6@gmail.com>
2025-08-09 16:18:53 -0500

    milestone

--- Angular/main-app/src/app/app.ts
+++ Angular/main-app/src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('main-app');
+  leftRailWidth = 150; // px
+
+  leftRailSrc: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.leftRailSrc = this.sanitizer.bypassSecurityTrustResourceUrl('/left-rail.html');
+  }
+
+  get leftRailWidthPx(): string {
+    return `${this.leftRailWidth}px`;
+  }
 }
